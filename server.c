2e71c8f5375b4968ad11f172c5b23d79b27ff600
manishramanandi <bairagimanish222@gmail.com>
2025-07-20 23:41:23 +0530

    write some javasript nerd

--- server.c
+++ server.c
@@ -3,7 +3,37 @@
 #include <sys/socket.h>
 #include <arpa/inet.h>
 #include <string.h>
+#include <sys/types.h>
 #include <unistd.h>
+#include <stdbool.h>
+
+int handle_client(int client_socket) {
+    ssize_t n = 0;
+    char buf[1024];
+    const char* hello = "HTTP/1.0 200 OK\r\n\r\n<h1>Hello, World!</h1>";
+
+    printf("\n-----\n");
+    for (;;) {
+    memset(buf, 0, sizeof(buf));
+
+    n = read(client_socket, buf, sizeof(buf)-1);
+    if (n < 0) {
+        perror("read(client");
+        return -1;
+    }
+    if (n == 0) {
+        printf("connection closed gracefully\n");
+            break;
+    }
+
+    printf("REQUEST:\n%s", buf);
+        (void)write(client_socket, hello, strlen(hello));
+        close(client_socket);
+        break;
+    }
+    printf("\n-----\n");
+    return 0;
+}
 
 int main(void) {
     /* declare */
@@ -12,6 +42,7 @@ int main(void) {
     int tcp_socket = 0;
     int ret = 0;
     int client_socket = 0;
+    int enabled = true;
 
     /* intialize */
     memset(&bind_addr, 0, sizeof(bind_addr));
@@ -27,6 +58,9 @@ int main(void) {
         return 1;
     }
     printf("Socket creation succesfull\n");
+
+    /* dont care if this fails */
+    (void)setsockopt(tcp_socket, SOL_SOCKET, SO_REUSEADDR, &enabled, sizeof(enabled));
 
     bind_addr.sin_port = htons(6969);
     bind_addr.sin_family = AF_INET;
@@ -53,7 +87,8 @@ int main(void) {
     printf("waiting for connection\n");
     client_socket = accept(tcp_socket, NULL, NULL);
     printf("got a connection\n");
-
+    rc = handle_client(client_socket);
+        /* ignore some errors, don't care for now */
     };
 
 exit:
